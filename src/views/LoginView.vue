4a7b946bfeba0d44f9b6608414af113b9efca9bb
rushionfisher <94405505+rushionfisher@users.noreply.github.com>
2023-04-28 22:43:30 -0500

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,19 +1,7 @@
 <template>
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <h1>Login</h1>
-          <Login />
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import Login from "../components/Login.vue";
-  
-  export default {
-    name: "Login",
-    components: { Login }
-  };
-  </script>
+  <Login/>
+</template>
+
+<script setup>
+  import Login from '@/components/Login.vue';
+</script>
